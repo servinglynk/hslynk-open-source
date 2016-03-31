3bf05a6b14d0b51d692f3bd28ca2bd3e2b149099
suryayadavalli <surya.yadavalli@gmail.com>
2016-03-31 14:10:49 +0530

    Removing unwanted files (eclipse workspace related files)

--- hmis-model/src/main/java/com/servinglynk/hmis/warehouse/dao/HMISTypeDao.java
+++
@@ -1,5 +0,0 @@
-package com.servinglynk.hmis.warehouse.dao;
-
-public interface HMISTypeDao {
-
-}
--- hmis-model/src/main/java/com/servinglynk/hmis/warehouse/dao/HMISTypeDaoImpl.java
+++
@@ -1,5 +0,0 @@
-package com.servinglynk.hmis.warehouse.dao;
-
-public class HMISTypeDaoImpl extends QueryExecutorImpl implements HMISTypeDao {
-
-}
