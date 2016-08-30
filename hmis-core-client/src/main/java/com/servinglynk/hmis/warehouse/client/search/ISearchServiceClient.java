191619905332346ee3d113e9b4bf81b387b9b0d7
suryayadavalli <surya.yadavalli@gmail.com>
2016-08-30 15:01:32 +0530

    Removing unused model

--- hmis-core-client/src/main/java/com/servinglynk/hmis/warehouse/client/search/ISearchServiceClient.java
+++ hmis-core-client/src/main/java/com/servinglynk/hmis/warehouse/client/search/ISearchServiceClient.java
@@ -3,7 +3,6 @@ package com.servinglynk.hmis.warehouse.client.search;
 import java.util.List;
 
 import com.servinglynk.hmis.warehouse.client.model.SearchRequest;
-import com.servinglynk.hmis.warehouse.client.model.SearchResults;
 
 public interface ISearchServiceClient {
 
