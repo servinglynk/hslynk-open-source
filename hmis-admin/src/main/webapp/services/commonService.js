48965e112e99ddadebfac67f7484d323c7c937a7
logicsandeep <sandeep.dolia@gmail.com>
2017-01-21 22:26:34 -0600

    Increasing the number of eligibility requirements we can get

--- hmis-admin/src/main/webapp/services/commonService.js
+++ hmis-admin/src/main/webapp/services/commonService.js
@@ -350,7 +350,7 @@ GetProjectGroups: function ($http,$scope, success) {
       });
 },
 GetEligReq: function ($http,$scope, success) {
-	  var apiurl = "/inventory-api/rest/projects/eligibilityrequirements?page=0&size=10";
+	  var apiurl = "/inventory-api/rest/projects/eligibilityrequirements?page=0&size=100";
 	 console.log('Session Token..'+$scope.sessionToken);
     $http({
         method: 'GET',
