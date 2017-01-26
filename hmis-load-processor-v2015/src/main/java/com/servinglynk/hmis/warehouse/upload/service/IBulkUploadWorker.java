8b34e2379b21df0a448472c1fda4639a2a96c0a4
logicsandeep <sandeep.dolia@gmail.com>
2017-01-26 17:09:16 -0600

    Some more progress in breaking the process into chunks

--- hmis-load-processor-v2015/src/main/java/com/servinglynk/hmis/warehouse/upload/service/IBulkUploadWorker.java
+++ hmis-load-processor-v2015/src/main/java/com/servinglynk/hmis/warehouse/upload/service/IBulkUploadWorker.java
@@ -4,4 +4,10 @@ package com.servinglynk.hmis.warehouse.upload.service;
 
 public interface IBulkUploadWorker  {
 	public void processWorkerLine() throws Exception;
+	public void processProjectChildren() throws Exception;
+	public void processEnrollmentChildren1() throws Exception;
+	public void processEnrollmentChildren2() throws Exception;
+	public void processEnrollmentChildren3() throws Exception;
+	public void processExitChildren1() throws Exception;
+	public void processDisabilities() throws Exception;
 }
