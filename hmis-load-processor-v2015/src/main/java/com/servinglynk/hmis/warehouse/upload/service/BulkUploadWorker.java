ac3e8042c6c7b8253a5a763e34ee433d3c323fe4
logicsandeep <sandeep.dolia@gmail.com>
2017-02-21 16:43:28 -0600

    Some progress in CSV 4.1

--- hmis-load-processor-v2015/src/main/java/com/servinglynk/hmis/warehouse/upload/service/BulkUploadWorker.java
+++ hmis-load-processor-v2015/src/main/java/com/servinglynk/hmis/warehouse/upload/service/BulkUploadWorker.java
@@ -45,7 +45,7 @@ public class BulkUploadWorker implements IBulkUploadWorker  {
 	@Scheduled(initialDelay=20,fixedDelay=10000)
 	public void processWorkerLine() throws BulkUploadException{
 		try {
-			List<BulkUpload> uploadEntities=  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			List<BulkUpload> uploadEntities=  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.INITIAL.getStatus(),new Long(2015));
 			if(uploadEntities!=null && uploadEntities.size() >0 ) {
 				for(BulkUpload upload : uploadEntities) {
 					FileAppender appender = new FileAppender();
@@ -82,6 +82,192 @@ public class BulkUploadWorker implements IBulkUploadWorker  {
 		}
 	}
 	
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processProjectChildren() throws BulkUploadException{
+	try {
+		List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+		if(uploadEntities!=null && uploadEntities.size() >0 ) {
+			for(BulkUpload upload : uploadEntities) {
+				FileAppender appender = new FileAppender();
+				appender.setName("" + upload.getId());
+				appender.setFile("logs/" + upload.getId() + "-processProjectChildren.log");
+				appender.setImmediateFlush(true);
+				appender.setAppend(true);
+				appender.setLayout(new PatternLayout());
+				appender.activateOptions();
+				upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+				factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+				logger.info("======== processProjectChildren processing ======");
+				ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+				factory.getBulkUploaderDao().processProjectChildren(upload,projectGroupEntity,appender,true);
+			}
+		}
+		logger.info("======== processProjectChildren processed ======");
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processEnrollmentChildren1() throws BulkUploadException{
+	try {
+		List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+		if(uploadEntities!=null && uploadEntities.size() >0 ) {
+			for(BulkUpload upload : uploadEntities) {
+				FileAppender appender = new FileAppender();
+				appender.setName("" + upload.getId());
+				appender.setFile("logs/" + upload.getId() + "-processEnrollmentChildren1.log");
+				appender.setImmediateFlush(true);
+				appender.setAppend(true);
+				appender.setLayout(new PatternLayout());
+				appender.activateOptions();
+				upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+				factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+				logger.info("======== processEnrollmentChildren1 processing ======");
+				ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+				factory.getBulkUploaderDao().processEnrollmentChildrenPart1(upload,projectGroupEntity,appender,true);
+			}
+		}
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processEnrollmentChildren2() throws BulkUploadException{
+		try {
+			List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			if(uploadEntities!=null && uploadEntities.size() >0 ) {
+				for(BulkUpload upload : uploadEntities) {
+					FileAppender appender = new FileAppender();
+					appender.setName("" + upload.getId());
+					appender.setFile("logs/" + upload.getId() + "-processEnrollmentChildren2.log");
+					appender.setImmediateFlush(true);
+					appender.setAppend(true);
+					appender.setLayout(new PatternLayout());
+					appender.activateOptions();
+					upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+					factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+					logger.info("======== processEnrollmentChildren2 processing ======");
+					ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+					factory.getBulkUploaderDao().processEnrollmentChildrenPart2(upload,projectGroupEntity,appender,true);
+				}
+			}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processEnrollmentChildren3() throws BulkUploadException{
+		try {
+			List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			if(uploadEntities!=null && uploadEntities.size() >0 ) {
+				for(BulkUpload upload : uploadEntities) {
+					FileAppender appender = new FileAppender();
+					appender.setName("" + upload.getId());
+					appender.setFile("logs/" + upload.getId() + "-processEnrollmentChildren3.log");
+					appender.setImmediateFlush(true);
+					appender.setAppend(true);
+					appender.setLayout(new PatternLayout());
+					appender.activateOptions();
+					upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+					logger.info("======== processEnrollmentChildren3 processing ======");
+					factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+					ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+					factory.getBulkUploaderDao().processEnrollmentChildrenPart3(upload,projectGroupEntity,appender,true);
+				}
+			}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processExitChildren1() throws BulkUploadException{
+		try {
+			List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			if(uploadEntities!=null && uploadEntities.size() >0 ) {
+				for(BulkUpload upload : uploadEntities) {
+					FileAppender appender = new FileAppender();
+					appender.setName("" + upload.getId());
+					appender.setFile("logs/" + upload.getId() + "-processExitChildren1.log");
+					appender.setImmediateFlush(true);
+					appender.setAppend(true);
+					appender.setLayout(new PatternLayout());
+					appender.activateOptions();
+					upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+					factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+					logger.info("======== processExitChildren1 processing ======");
+					ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+					factory.getBulkUploaderDao().processExitChildrenPart1(upload,projectGroupEntity,appender,true);
+				}
+			}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void processDisabilities() throws BulkUploadException{
+		try {
+			List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			if(uploadEntities!=null && uploadEntities.size() >0 ) {
+				for(BulkUpload upload : uploadEntities) {
+					FileAppender appender = new FileAppender();
+					appender.setName("" + upload.getId());
+					appender.setFile("logs/" + upload.getId() + "-processDisabilities.log");
+					appender.setImmediateFlush(true);
+					appender.setAppend(true);
+					appender.setLayout(new PatternLayout());
+					appender.activateOptions();
+					upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+					factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+					logger.info("======== processDisabilities processing ======");
+					ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+					factory.getBulkUploaderDao().processDisabilities(upload,projectGroupEntity,appender,true);
+				}
+			}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+	
+	@Transactional
+	@Scheduled(initialDelay=20,fixedDelay=10000)
+	public void calculateChronicHomelessness() throws BulkUploadException{
+		try {
+			List<BulkUpload> uploadEntities =  factory.getBulkUploaderWorkerDao().findBulkUploadByStatusAndYear(UploadStatus.CORE.getStatus(),new Long(2015));
+			if(uploadEntities!=null && uploadEntities.size() >0 ) {
+				for(BulkUpload upload : uploadEntities) {
+					FileAppender appender = new FileAppender();
+					appender.setName("" + upload.getId());
+					appender.setFile("logs/" + upload.getId() + "-calculateChronicHomelessness.log");
+					appender.setImmediateFlush(true);
+					appender.setAppend(true);
+					appender.setLayout(new PatternLayout());
+					appender.activateOptions();
+					upload.setStatus(UploadStatus.INPROGRESS.getStatus());
+					factory.getBulkUploaderWorkerDao().insertOrUpdate(upload);
+					logger.info("======== processDisabilities processing ======");
+					ProjectGroupEntity projectGroupEntity = factory.getProjectGroupDao().getProjectGroupByGroupCode(upload.getProjectGroupCode());
+					factory.getBulkUploaderDao().calculateChronicHomelessness(projectGroupEntity.getProjectGroupCode());(
+				}
+			}
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	private void moveFile(String inputPath,String outputPath) throws IOException
 	{
 		
