fe374f97c510a897083defcf74df852fdceaa500
Sandeep Dolia <sdolia@Sandeeps-MacBook-Pro.local>
2017-05-09 15:58:47 -0500

    Some more progress in HUD Reports

--- hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q06bBeanMaker.java
+++ hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q06bBeanMaker.java
@@ -4,19 +4,32 @@ import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
 
+import com.servinglynk.report.bean.ClientModel;
 import com.servinglynk.report.bean.Q06bNumberOfPersonsServedDataBean;
 import com.servinglynk.report.bean.ReportData;
 
+import jodd.util.StringUtil;
+
 public class Q06bBeanMaker  {
+	public static Long veteranStatusErrorCount = 0L;
 	
 	public static List<Q06bNumberOfPersonsServedDataBean> getQ06bNumberOfPersonsServedTableList(ReportData data){
 		Q06bNumberOfPersonsServedDataBean q06bNumberOfPersonsServedDataBean = new Q06bNumberOfPersonsServedDataBean();
+		List<ClientModel> clients = data.getClients();
+		Long numOfClients = Long.valueOf(clients.size());
+		clients.parallelStream().forEach(client -> { 
+			
+			if(StringUtil.equals("99", client.getName_data_quality())) {
+				veteranStatusErrorCount++;
+			}
+		}
+		);
 		
 	//	q06aReportValidationsTableBean.setNumOfAdults(BigInteger.valueOf(adults !=null ?adults.size() : 0));
 	//	q06aReportValidationsTableBean.setNumOfChildren(BigInteger.valueOf(children !=null ? children.size() : 0));
 		
 		q06bNumberOfPersonsServedDataBean.setVeteranStatusErrorCount(BigInteger.valueOf(0));
-		q06bNumberOfPersonsServedDataBean.setVeteranStatusErrorRate(BigInteger.valueOf(0));
+		q06bNumberOfPersonsServedDataBean.setVeteranStatusErrorRate(BigInteger.valueOf(numOfClients/veteranStatusErrorCount));
 		q06bNumberOfPersonsServedDataBean.setPedErrorCount(BigInteger.valueOf(0));
 		q06bNumberOfPersonsServedDataBean.setPedErrorRate(BigInteger.valueOf(0));
 		q06bNumberOfPersonsServedDataBean.setRelationshipHHErrorCount(BigInteger.valueOf(0));
