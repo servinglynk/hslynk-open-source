665651cdbf9b07ef6578e5a4bd8007d891b058dd
Sandeep Dolia <sdolia@Sandeeps-MacBook-Pro.local>
2017-08-29 02:55:36 -0500

    process with extracting bulk upload from 7z file

--- hmis-base-model/src/main/java/com/servinglynk/hmis/warehouse/base/util/ExtractItemsSimple.java
+++ hmis-base-model/src/main/java/com/servinglynk/hmis/warehouse/base/util/ExtractItemsSimple.java
@@ -6,20 +6,31 @@ import java.io.IOException;
 
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.io.FileUtils;
 
 public class ExtractItemsSimple {
-    public static void main(String[] args) throws IOException {
-    	SevenZFile sevenZFile = new SevenZFile(new File("/Users/sdolia/Downloads/test.7z"));
+    public static void main(String[] args) {
+    	extractFiles("/Users/sdolia/Downloads/2016.xml.7z", "2");
+    }
+    
+    public static void extractFiles(String sevenzPath,String bulkUploadId) {
+    	SevenZFile sevenZFile;
+		try {
+			sevenZFile = new SevenZFile(new File(sevenzPath));
         SevenZArchiveEntry entry = sevenZFile.getNextEntry();
-        while(entry!=null){
+        while(entry != null){
             System.out.println(entry.getName());
-            FileOutputStream out = new FileOutputStream(entry.getName());
+            new File(bulkUploadId).mkdir();
+            FileOutputStream out = new FileOutputStream(bulkUploadId+"/"+entry.getName());
             byte[] content = new byte[(int) entry.getSize()];
             sevenZFile.read(content, 0, content.length);
             out.write(content);
             out.close();
             entry = sevenZFile.getNextEntry();
-        }
-        sevenZFile.close();
+         }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
