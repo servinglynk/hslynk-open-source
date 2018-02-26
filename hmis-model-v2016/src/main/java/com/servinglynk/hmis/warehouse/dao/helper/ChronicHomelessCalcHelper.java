5492387d1846791384a9ac256d1fb1acd6735072
Sandeep Dolia <sdolia@Sandeeps-MacBook-Pro.local>
2018-02-25 23:31:25 -0600

    Some more changes to CH calculation

--- hmis-model-v2016/src/main/java/com/servinglynk/hmis/warehouse/dao/helper/ChronicHomelessCalcHelper.java
+++ hmis-model-v2016/src/main/java/com/servinglynk/hmis/warehouse/dao/helper/ChronicHomelessCalcHelper.java
@@ -1,6 +1,5 @@
 package com.servinglynk.hmis.warehouse.dao.helper;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Set;
@@ -9,13 +8,14 @@ import org.springframework.stereotype.Component;
 
 import com.servinglynk.hmis.warehouse.enums.DisabilitiesIndefiniteandimpairsEnum;
 import com.servinglynk.hmis.warehouse.enums.EnrollmentDisablingconditionEnum;
-import com.servinglynk.hmis.warehouse.enums.EnrollmentMonthshomelesspastthreeyearsEnum;
 import com.servinglynk.hmis.warehouse.enums.EnrollmentResidencepriorEnum;
 import com.servinglynk.hmis.warehouse.enums.EnrollmentResidencepriorlengthofstayEnum;
 import com.servinglynk.hmis.warehouse.enums.EnrollmentTimeshomelesspastthreeyearsEnum;
 import com.servinglynk.hmis.warehouse.enums.ProjectProjecttypeEnum;
+import com.servinglynk.hmis.warehouse.enums.RecordTypeEnum;
 import com.servinglynk.hmis.warehouse.model.v2016.Disabilities;
 import com.servinglynk.hmis.warehouse.model.v2016.Enrollment;
+import com.servinglynk.hmis.warehouse.model.v2016.ServiceFaReferral;
 
 @Component
 public class ChronicHomelessCalcHelper{
@@ -34,38 +34,71 @@ public class ChronicHomelessCalcHelper{
 	public boolean isEnrollmentChronicHomeless(Enrollment enrollment) {
 		if(enrollment !=null) {
 			if((EnrollmentDisablingconditionEnum.ONE.equals(enrollment.getDisablingcondition()))) {
+				
 				if(enrollment.getProject() !=null && (ProjectProjecttypeEnum.ONE.equals(enrollment.getProject().getProjecttype()) || ProjectProjecttypeEnum.FOUR.equals(enrollment.getProject().getProjecttype()) || ProjectProjecttypeEnum.EIGHT.equals(enrollment.getProject().getProjecttype()))) {
-					return validateDiffInDays(enrollment);
+					return validateDiffInDays(enrollment,enrollment.getEntrydate());
 				}
+				
+			    if(EnrollmentResidencepriorEnum.ONE.equals(enrollment.getResidenceprior())
+					  || EnrollmentResidencepriorEnum.SIXTEEN.equals(enrollment.getResidenceprior())
+					  || EnrollmentResidencepriorEnum.EIGHTEEN.equals(enrollment.getResidenceprior())
+					  || EnrollmentResidencepriorEnum.TWENTY_SEVEN.equals(enrollment.getResidenceprior())) {
+						return validateDiffInDays(enrollment,enrollment.getEntrydate());
+				}
+			    
+			    if(EnrollmentResidencepriorEnum.FIFTEEN.equals(enrollment.getResidenceprior())
+							|| EnrollmentResidencepriorEnum.SIX.equals(enrollment.getResidenceprior())
+							|| EnrollmentResidencepriorEnum.SEVEN.equals(enrollment.getResidenceprior())
+							|| EnrollmentResidencepriorEnum.TWENTY_FOUR.equals(enrollment.getResidenceprior())
+							|| EnrollmentResidencepriorEnum.FOUR.equals(enrollment.getResidenceprior())
+							|| EnrollmentResidencepriorEnum.FIVE.equals(enrollment.getResidenceprior())) {
+						if(enrollment.getResidencepriorlengthofstay() !=null 
+								&& ( EnrollmentResidencepriorlengthofstayEnum.TWO.equals(enrollment.getResidencepriorlengthofstay()) 
+								  || EnrollmentResidencepriorlengthofstayEnum.THREE.equals(enrollment.getResidencepriorlengthofstay()) 
+								  || EnrollmentResidencepriorlengthofstayEnum.TEN.equals(enrollment.getResidencepriorlengthofstay()) 
+								  || EnrollmentResidencepriorlengthofstayEnum.ELEVEN.equals(enrollment.getResidencepriorlengthofstay()))) {
+							return oNightBeforeESSH(enrollment);
+						}
+				}
+
+			    if(EnrollmentResidencepriorEnum.THREE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWO.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWELVE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.THIRTEEN.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.FOURTEEN.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.NINTEEN.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY_ONE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY_TWO.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY_THREE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY_FIVE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.TWENTY_SIX.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.EIGHT.equals(enrollment.getResidenceprior()) 
+						|| EnrollmentResidencepriorEnum.NINE.equals(enrollment.getResidenceprior())
+						|| EnrollmentResidencepriorEnum.NINTY_NINE.equals(enrollment.getResidenceprior())) {
+						if(enrollment.getResidencepriorlengthofstay() !=null 
+								&& ( EnrollmentResidencepriorlengthofstayEnum.TEN.equals(enrollment.getResidencepriorlengthofstay()) 
+								  || EnrollmentResidencepriorlengthofstayEnum.ELEVEN.equals(enrollment.getResidencepriorlengthofstay()) )) {
+									return oNightBeforeESSH(enrollment);
+						     }
+			    	}
+		  	  }
 			}
-			if(enrollment.getResidenceprior() !=null &&   ( EnrollmentResidencepriorEnum.EIGHT.equals(enrollment.getResidenceprior()) 
-			  || EnrollmentResidencepriorEnum.NINE.equals(enrollment.getResidenceprior())
-			  || EnrollmentResidencepriorEnum.NINTY_NINE.equals(enrollment.getResidenceprior()))){
-				return false;
-			}else {
-				if(EnrollmentResidencepriorEnum.ONE.equals(enrollment.getResidenceprior())
-				  || EnrollmentResidencepriorEnum.SIXTEEN.equals(enrollment.getResidenceprior())
-				  || EnrollmentResidencepriorEnum.EIGHTEEN.equals(enrollment.getResidenceprior())
-				  || EnrollmentResidencepriorEnum.TWENTY_SEVEN.equals(enrollment.getResidenceprior())) {
-					return validateDiffInDays(enrollment);
-				}else if(EnrollmentResidencepriorEnum.FIFTEEN.equals(enrollment.getResidenceprior())
-						|| EnrollmentResidencepriorEnum.SIX.equals(enrollment.getResidenceprior())
-						|| EnrollmentResidencepriorEnum.SEVEN.equals(enrollment.getResidenceprior())
-						|| EnrollmentResidencepriorEnum.TWENTY_FOUR.equals(enrollment.getResidenceprior())
-						|| EnrollmentResidencepriorEnum.FOUR.equals(enrollment.getResidenceprior())
-						|| EnrollmentResidencepriorEnum.FIVE.equals(enrollment.getResidenceprior())) {
-					if(enrollment.getResidencepriorlengthofstay() !=null 
-							&& EnrollmentResidencepriorlengthofstayEnum.THREE.equals(enrollment.getResidencepriorlengthofstay())
-							) {
-						 return validateDiffInDays(enrollment);
+		return false;
+	}
+	
+
+	private boolean oNightBeforeESSH(Enrollment enrollment) {
+		for(ServiceFaReferral service : enrollment.getServiceFaReferrals()) {
+			RecordTypeEnum recordType = service.getRecordType();
+			if(RecordTypeEnum.THIRTEEN.equals(recordType)) {
+				Integer typeProvided = service.getTypeProvided();
+				if(typeProvided != null) {
+					if(typeProvided == 1) {
+						return validateDiffInDays(enrollment,enrollment.getEntrydate());
 					}
-						
-					
 				}
-				
-				
 			}
-			
 		}
 		return false;
 	}
@@ -74,8 +107,8 @@ public class ChronicHomelessCalcHelper{
 		  return ChronoUnit.DAYS.between(a, localDateTime);
 		}
 	
-	private boolean validateDiffInDays(Enrollment enrollment) {
-		if(diffInDays(LocalDateTime.now(),enrollment.getEntrydate()) > 365) {
+	private boolean validateDiffInDays(Enrollment enrollment, LocalDateTime date) {
+		if(enrollment.getEntrydate() !=null && diffInDays(LocalDateTime.now(),date) > 365) {
 			return true;
 		}else {
 			return validateTimesHomeless(enrollment);
