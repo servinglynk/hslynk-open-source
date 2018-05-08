325c70d96263cfe2ec30d3e974032cf952bbe30d
logicsandeep <sandeep.dolia@gmail.com>
2018-05-08 00:59:57 -0500

    Apr Section 11a

--- hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q11BeanMaker.java
+++ hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q11BeanMaker.java
@@ -38,6 +38,12 @@ public class Q11BeanMaker extends BaseBeanMaker {
 				List<String> projectsHHWithChildren = data.getProjectsHHWithChildren();
 				List<String> projectsUnknownHouseHold = data.getProjectsUnknownHouseHold();
 				
+				BigInteger  totalUHHT = BigInteger.ZERO;
+				BigInteger	totalWCA = BigInteger.ZERO;
+				BigInteger	totalWithOnlyChild = BigInteger.ZERO;
+				BigInteger	totalWOC = BigInteger.ZERO;
+				BigInteger  totOverallTotal = BigInteger.ZERO;
+				
 				Q11AgeDataBean q11AgeDataBean = new Q11AgeDataBean();
 				if(CollectionUtils.isNotEmpty(under5Tot)) {
 					List<String> filteredClients = new ArrayList<>();
@@ -46,11 +52,18 @@ public class Q11BeanMaker extends BaseBeanMaker {
 					List<EnrollmentModel> clientWCA = enrollments.parallelStream().filter(enrollment-> enrollment.getPersonalID() != null && filteredClients.contains(enrollment.getPersonalID())  && enrollment.getProjectID() != null && projectsHHWithOneAdultChild.contains(enrollment.getProjectID())).collect(Collectors.toList());
 					List<EnrollmentModel> clientUHHT = enrollments.parallelStream().filter(enrollment-> enrollment.getPersonalID() != null && filteredClients.contains(enrollment.getPersonalID())  && enrollment.getProjectID() != null && projectsUnknownHouseHold.contains(enrollment.getProjectID())).collect(Collectors.toList());
 					List<EnrollmentModel> clientWOnlyC = enrollments.parallelStream().filter(enrollment-> enrollment.getPersonalID() != null && filteredClients.contains(enrollment.getPersonalID())  && enrollment.getProjectID() != null && projectsHHWithChildren.contains(enrollment.getProjectID())).collect(Collectors.toList());
+					
 					q11AgeDataBean.setTotAgeUnder5(BigInteger.valueOf(under5Tot !=null ?under5Tot.size() : 0));
 					q11AgeDataBean.setAgeUnder5UHHT(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0 ));
 		    		q11AgeDataBean.setAgeUnder5WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size() :0));
 		    		q11AgeDataBean.setAgeUnder5WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size() :0));
 		    		q11AgeDataBean.setAgeUnder5WOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(under5Tot.size()));
+		    		
 				}
 			
 				if(CollectionUtils.isNotEmpty(clients5To12)) {
@@ -66,6 +79,12 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge5to12WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size() :0));
 		    		q11AgeDataBean.setAge5to12WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge5to12WOC(BigInteger.valueOf(clientWOC != null ? clientWOC.size() :0));
+		    		
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients5To12.size()));
 				}
 	    		
 				if(CollectionUtils.isNotEmpty(clients13To17)) {
@@ -81,6 +100,13 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge13to17WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAge13to17WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size() :0));
 		    		q11AgeDataBean.setAge13to17WOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients13To17.size()));
+				
 				}
 				
 				if(CollectionUtils.isNotEmpty(clients18To24)) {
@@ -97,6 +123,11 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge18to24WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null?clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge18to24WOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
 		    		
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients18To24.size()));
 				}
 				
 				if(CollectionUtils.isNotEmpty(clients25To34)) {
@@ -112,6 +143,12 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge25to34WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size() :0));
 		    		q11AgeDataBean.setAge25to34WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size() :0));
 		    		q11AgeDataBean.setAge25to34WOC(BigInteger.valueOf(clientWOC != null ? clientWOC.size():0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients25To34.size()));
 				}
 
 				if(CollectionUtils.isNotEmpty(clients35To44)) {
@@ -127,6 +164,13 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge35to44WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size() :0));
 		    		q11AgeDataBean.setAge35to44WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge35to44WOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
+				
+		      		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients35To44.size()));
+		    		
 				}
 				if(CollectionUtils.isNotEmpty(clients45To54)) {
 					List<String> filteredClients = new ArrayList<>();
@@ -141,6 +185,13 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge45to54WCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAge45to54WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null ?clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge45to54WOC(BigInteger.valueOf(clientWOC != null?clientWOC.size():0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients45To54.size()));
+				
 				}
 				if(CollectionUtils.isNotEmpty(clients55To61)) {
 					List<String> filteredClients = new ArrayList<>();
@@ -155,6 +206,12 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge55to61WCA(BigInteger.valueOf( clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAge55to61WithOnlyChild(BigInteger.valueOf(clientWOnlyC != null? clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge55to61WOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clients55To61.size()));
 				}
 				
 				if(CollectionUtils.isNotEmpty(clientsOver61)) {
@@ -170,6 +227,13 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAge62PlusWCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAge62PlusWithOnlyChild(BigInteger.valueOf(clientWOnlyC != null? clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAge62PlusWOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clientsOver61.size()));
+		    		
 				}
 				if(CollectionUtils.isNotEmpty(clientsDNC)) {
 					List<String> filteredClients = new ArrayList<>();
@@ -184,6 +248,12 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAgeDKRWCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAgeDKRWithOnlyChild(BigInteger.valueOf(clientWOnlyC != null? clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAgeDKRWOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
+				
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clientsDNC.size()));
 				}
 				
 				if(CollectionUtils.isNotEmpty(clientsDidnotknowRefused)) {
@@ -199,13 +269,14 @@ public class Q11BeanMaker extends BaseBeanMaker {
 		    		q11AgeDataBean.setAgeInfoMissingWCA(BigInteger.valueOf(clientWCA != null ?clientWCA.size():0));
 		    		q11AgeDataBean.setAgeInfoMissingWithOnlyChild(BigInteger.valueOf(clientWOnlyC != null? clientWOnlyC.size():0));
 		    		q11AgeDataBean.setAgeInfoMissingWOC(BigInteger.valueOf(clientWOC != null ?clientWOC.size():0));
+		    	
+		    		totalUHHT.add(BigInteger.valueOf(clientUHHT != null ? clientUHHT.size() :0));
+		    		totalWCA.add(BigInteger.valueOf(clientWCA != null ? clientWCA.size() :0));
+		    		totalWithOnlyChild.add(BigInteger.valueOf(clientWOnlyC != null ? clientWOnlyC.size() :0));
+		    		totalWOC.add(BigInteger.valueOf(clientWOC != null ?clientWOC.size() :0));
+		    		totOverallTotal.add(BigInteger.valueOf(clientsDidnotknowRefused.size()));
 				}
 	    		
-				BigInteger  totalUHHT = BigInteger.ZERO;
-				BigInteger	totalWCA = BigInteger.ZERO;
-				BigInteger	totalWithOnlyChild = BigInteger.ZERO;
-				BigInteger	totalWOC = BigInteger.ZERO;
-				BigInteger  totOverallTotal = BigInteger.ZERO;
 	    		q11AgeDataBean.setAgeOverallTotalUHHT(totalUHHT);
 	    		q11AgeDataBean.setAgeOverallTotalWCA(totalWCA);
 	    		q11AgeDataBean.setAgeOverallTotalWithOnlyChild(totalWithOnlyChild);
