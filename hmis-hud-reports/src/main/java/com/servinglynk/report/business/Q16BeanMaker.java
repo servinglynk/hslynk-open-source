13dd4c5731d969e0f0961e156611965497b37e33
logicsandeep <sandeep.dolia@gmail.com>
2018-06-05 13:16:42 -0500

    Q16 Apr

--- hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q16BeanMaker.java
+++ hmis-hud-reports/src/main/java/com/servinglynk/report/business/Q16BeanMaker.java
@@ -21,61 +21,61 @@ public class Q16BeanMaker {
 		
 		int stayers = clients !=null && exits != null ? clients.size() - exits.size() : 0;*/
 		
-		Q16CashIncomeRangesDataBean q16CashIncomeRangesTableBean = new Q16CashIncomeRangesDataBean();
+		Q16CashIncomeRangesDataBean q16Bean = new Q16CashIncomeRangesDataBean();
 		
-		q16CashIncomeRangesTableBean.setQ16NoIncomeIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NoIncomeIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NoIncomeIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16NoIncomeIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16NoIncomeIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16NoIncomeIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ161To150IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161To150IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161To150IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ161To150IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ161To150IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ161To150IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16151To250IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16151To250IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16151To250IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16151To250IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16151To250IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16151To250IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16251To500IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16251To500IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16251To500IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16251To500IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16251To500IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16251To500IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16501To1000IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16501To1000IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16501To1000IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16501To1000IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16501To1000IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16501To1000IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ161001To1500IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161001To1500IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161001To1500IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ161001To1500IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ161001To1500IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ161001To1500IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ161501To2000IncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161501To2000IncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ161501To2000IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ161501To2000IncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ161501To2000IncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ161501To2000IncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ162000PlusIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ162000PlusIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ162000PlusIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ162000PlusIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ162000PlusIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ162000PlusIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16ClientDoesntKnowIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16ClientDoesntKnowIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16ClientDoesntKnowIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16ClientDoesntKnowIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16ClientDoesntKnowIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16ClientDoesntKnowIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16DataNotCollectedIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16DataNotCollectedIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16DataNotCollectedIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16DataNotCollectedIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16DataNotCollectedIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16DataNotCollectedIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersNotYetRequiredIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 		
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtEntry(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtExitforLeavers(BigInteger.valueOf(0));
-		q16CashIncomeRangesTableBean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtEntry(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtExitforLeavers(BigInteger.valueOf(0));
+		q16Bean.setQ16NumberOfAdultStayersWithoutRequiredIncomeAtLatestFollowupforStayers(BigInteger.valueOf(0));
 
-		q16CashIncomeRangesTableBean.setQ16TotalAdultsIncomeAtEntry(data.getNumOfAdults());
-		q16CashIncomeRangesTableBean.setQ16TotalAdultsIncomeAtExitforLeavers(data.getTotNoOfAdultLeavers());
-		q16CashIncomeRangesTableBean.setQ16TotalAdultsIncomeAtLatestFollowupforStayers(data.getTotNoOfAdultStayers());
+		q16Bean.setQ16TotalAdultsIncomeAtEntry(data.getNumOfAdults());
+		q16Bean.setQ16TotalAdultsIncomeAtExitforLeavers(data.getTotNoOfAdultLeavers());
+		q16Bean.setQ16TotalAdultsIncomeAtLatestFollowupforStayers(data.getTotNoOfAdultStayers());
 		
-		return Arrays.asList(q16CashIncomeRangesTableBean);
+		return Arrays.asList(q16Bean);
 	}
 
 }
