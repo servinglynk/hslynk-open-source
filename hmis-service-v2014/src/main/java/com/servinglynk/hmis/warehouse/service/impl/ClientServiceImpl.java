9c1569299ef61491240f3e9c84a09e9b4273c4c7
suryayadavalli <surya.yadavalli@gmail.com>
2018-06-28 12:28:07 +0530

    Fix for https://github.com/servinglynk/hslynk-open-source-docs/issues/528

--- hmis-service-v2014/src/main/java/com/servinglynk/hmis/warehouse/service/impl/ClientServiceImpl.java
+++ hmis-service-v2014/src/main/java/com/servinglynk/hmis/warehouse/service/impl/ClientServiceImpl.java
@@ -3,6 +3,7 @@ package com.servinglynk.hmis.warehouse.service.impl;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
@@ -12,6 +13,7 @@ import com.servinglynk.hmis.warehouse.SortedPagination;
 import com.servinglynk.hmis.warehouse.core.model.BaseClient;
 import com.servinglynk.hmis.warehouse.core.model.Client;
 import com.servinglynk.hmis.warehouse.core.model.Clients;
+import com.servinglynk.hmis.warehouse.model.v2014.Enrollment;
 import com.servinglynk.hmis.warehouse.service.ClientService;
 import com.servinglynk.hmis.warehouse.service.converter.ClientConverter;
 import com.servinglynk.hmis.warehouse.service.exception.ClientNotFoundException;
@@ -59,7 +61,14 @@ public class ClientServiceImpl extends ServiceBase implements ClientService {
 		
 		if(pClient == null ) throw new ClientNotFoundException();
 		
+		Set<Enrollment> enrollments = pClient.getEnrollments();
+		
+		for(Enrollment enrollment : enrollments) {
+			daoFactory.getEnrollmentDao().delete(enrollment);
+		}
+		
 		com.servinglynk.hmis.warehouse.model.base.Client baseClient =  daoFactory.getBaseClientDao().getClient(clientId);
+
 		daoFactory.getClientDao().deleteClient(pClient);
 		if(baseClient!=null)
 			daoFactory.getHmisClientDao().deleteClient(baseClient);
