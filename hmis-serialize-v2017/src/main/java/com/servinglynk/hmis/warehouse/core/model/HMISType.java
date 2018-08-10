a0c6e9b91189b1890fb0dc2f6912b1a2660cff13
suryayadavalli <surya.yadavalli@gmail.com>
2018-08-10 13:04:37 +0530

    Updating json property names in picklist values in questions endpoint

--- hmis-serialize-v2017/src/main/java/com/servinglynk/hmis/warehouse/core/model/HMISType.java
+++ hmis-serialize-v2017/src/main/java/com/servinglynk/hmis/warehouse/core/model/HMISType.java
@@ -1,12 +1,17 @@
 package com.servinglynk.hmis.warehouse.core.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 @JsonRootName("dataElement")
 public class HMISType extends ClientModel {
-
+	
+	@JsonIgnore
 	public String name;
+	@JsonProperty("pickListValueCode")
 	public String value;
+	@JsonProperty("valueText")
 	public String description;
 	public String getName() {
 		return name;
