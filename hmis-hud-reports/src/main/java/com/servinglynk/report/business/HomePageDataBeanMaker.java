edf4edb7b3487cc9bc4dd6a5e136f3348b36af94
sandeepdolia <sandeep.dolia@gmail.com>
2020-02-18 14:53:42 -0500

    Section 4a refactoring

--- hmis-hud-reports/src/main/java/com/servinglynk/report/business/HomePageDataBeanMaker.java
+++ hmis-hud-reports/src/main/java/com/servinglynk/report/business/HomePageDataBeanMaker.java
@@ -3,9 +3,7 @@ package com.servinglynk.report.business;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections.CollectionUtils;
