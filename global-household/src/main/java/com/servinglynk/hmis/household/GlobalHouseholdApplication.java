8082b02a208297da4bf2149030d51b2d8906b668
suryayadavalli <surya.yadavalli@gmail.com>
2020-05-27 14:38:58 +0530

    Bug fix

--- global-household/src/main/java/com/servinglynk/hmis/household/GlobalHouseholdApplication.java
+++ global-household/src/main/java/com/servinglynk/hmis/household/GlobalHouseholdApplication.java
@@ -3,15 +3,18 @@ package com.servinglynk.hmis.household;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.servinglynk.hmis.household.repository.BaseRepositoryFactoryBean;
 import com.servinglynk.hmis.warehouse.client.config.SpringConfig;
+import com.servinglynk.hmis.warehouse.config.AMQConfiguration;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryFactoryBeanClass=BaseRepositoryFactoryBean.class)
 @EnableTransactionManagement
+@Import(AMQConfiguration.class)
 public class GlobalHouseholdApplication extends org.springframework.boot.web.support.SpringBootServletInitializer {
 
     @Override
