3522672ad37d20cf483d68ae09d08b5ad23cd984
sandeepdolia <sandeep.dolia@gmail.com>
2020-08-30 06:49:35 -0400

    first commit

--- hmis-mq-service/src/main/java/com/servinglynk/hmis/warehouse/service/HmisPostingServiceImpl.java
+++ hmis-mq-service/src/main/java/com/servinglynk/hmis/warehouse/service/HmisPostingServiceImpl.java
@@ -1,17 +1,27 @@
 package com.servinglynk.hmis.warehouse.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.servinglynk.hmis.warehouse.model.HmisPostingModel;
+import com.servinglynk.hmis.warehouse.model.QuestionResponseModel;
 import com.servinglynk.hmis.warehouse.model.SessionModel;
 
 @Service
 public class HmisPostingServiceImpl implements HmisPostingService {
 
 	@Override
-	public void postHmis(HmisPostingModel HmisPostingModel, SessionModel session) {
-		// TODO Auto-generated method stub
-		
+	public void postHmis(HmisPostingModel hmisPostingModel, SessionModel session) {
+			if(hmisPostingModel != null) {
+				List<QuestionResponseModel> questionResponses = hmisPostingModel.getQuestionResponses();
+				if(!CollectionUtils.isEmpty(questionResponses)) {
+					for( QuestionResponseModel questionResponseModel : questionResponses) {
+						System.out.println(questionResponseModel.getQuestionId());
+					}
+				}
+			}
 	}
 
 }
