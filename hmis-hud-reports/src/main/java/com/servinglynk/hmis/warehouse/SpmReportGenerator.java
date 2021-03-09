d7705a8e3762022dc694726591b9b7148cddf57d
sandeepdolia <sandeep.dolia@gmail.com>
2021-03-09 10:14:58 -0500

    Bug fixes

--- hmis-hud-reports/src/main/java/com/servinglynk/hmis/warehouse/SpmReportGenerator.java
+++ hmis-hud-reports/src/main/java/com/servinglynk/hmis/warehouse/SpmReportGenerator.java
@@ -1,5 +1,44 @@
 package com.servinglynk.hmis.warehouse;
 
-public class SpmReportGenerator {
+import org.apache.log4j.Logger;
 
+import com.servinglynk.hive.connection.SyncPostgresProcessor;
+
+public class SPMReportGenerator  extends Logging {
+	private static Logger logger = Logger.getLogger(LSAReportGenerator.class);
+	public SPMReportGenerator(Logger logger) throws Exception {
+	        this.logger = logger;
+	}
+	/***
+	 * Generate SPM report
+	 */
+	 public static void generateReport() {
+	        try {         
+	        	Properties props = new Properties();
+	    		props.generatePropValues();
+	    		logger.info("Starting SPM reports process....");
+	    		ReportConfig reportConfig = SyncPostgresProcessor.getReportConfigByStatusReportType("INITIAL","SPM");
+	    		if(reportConfig != null) {
+	    			SyncPostgresProcessor.updateReportConfig("INPROGRESS", reportConfig.getId());
+	    			String reportId = String.valueOf(reportConfig.getId());
+	        		SyncPostgresProcessor.updateReportConfig("BEFORE_ZIP_CREATION", reportConfig.getId());
+	            	  // Get the bucket name from project group code.
+	            	ZipFileProcessor.createZipFile(props.SPM_FILE_LOCATION, "", props.SPM_FILE_LOCATION+"/"+reportId+".zip");
+	            	SyncPostgresProcessor.updateReportConfig("BEFORE_S3", reportConfig.getId());
+	    		    String bucketName = SyncPostgresProcessor.getBucketName(reportConfig.getProjectGroupCode());
+	    		    AwsS3Client client = new AwsS3Client();
+	    			client.uploadFile(props.SPM_FILE_LOCATION+"/"+reportId+".zip", "SPM/"+reportId+".zip",bucketName);
+	    			// update the report config to 
+	    			SyncPostgresProcessor.updateReportConfig("COMPLETED", reportConfig.getId());
+	    		}
+	        } catch (Exception e) {
+	        	e.printStackTrace();
+	            logger.error(e, e);
+	        }
+	 }
+	 
+	public static void main(String args[]) {
+		generateReport();
+	}
+	
 }
